a289badc9608752f1a15b62f0f9a588e0c830cfd
hani <hanisoft2022@gmail.com>
2025-07-28 21:01:55 +0900

    ongoing..

--- packages/authentication_repository/lib/src/models/user.dart
+++ packages/authentication_repository/lib/src/models/user.dart
@@ -1,27 +1,13 @@
 import 'package:equatable/equatable.dart';
 
-/// {@template user}
-/// User model
-///
-/// [User.empty] represents an unauthenticated user.
-/// {@endtemplate}
 class User extends Equatable {
-  /// The current user's id.
   final String id;
-
-  /// The current user's email address.
   final String? email;
-
-  /// The current user's name (display name).
   final String? name;
-
-  /// Url for the current user's photo.
   final String? photo;
 
-  /// {@macro user}
   const User({required this.id, this.email, this.name, this.photo});
 
-  /// Empty user which represents an unauthenticated user.
   static const empty = User(id: '');
 
   @override
